a9ca894c6b065c7467432da7f3f81610178d12ce
Saúl <saul.franco@tucanton.com>
2022-05-25 00:36:49 -0500

    Se hace el maquetado de la pagina principal

--- src/components/Homepage/HomePage.component.jsx
+++ src/components/Homepage/HomePage.component.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
-
+import React                from 'react';
+import {
+    WrapperHomePage
+}                           from './HomePage.styles';
+import ImagePlus            from '../../Images/plus_report.png';
+import ImageViewReport      from '../../Images/report_section_1.png';
+import ImageViewProduct     from '../../Images/products_section_1.png';
+import ImageViewWaiters     from '../../Images/waiters_section_1.png';
+import ImageViewCashier     from '../../Images/cashier_section_1.png';
+import ImageViewZone        from '../../Images/zone_section_1.png';
 
 const HomePage = () => {
     return ( 
-        <>
-            <h1>Inicio</h1>
-        </>
+        <WrapperHomePage>
+            <div className='button_section background_black'>
+                <img
+                    className='image_button'
+                    src={ImagePlus}
+                    alt="crear reporte"
+                />
+                <h3 className='text_button'>
+                    Crear reporte
+                </h3>
+            </div>
+
+            <div className='button_section background_blue'>
+                <img
+                    className='image_button'
+                    src={ImageViewReport}
+                    alt="ver mis reportes"
+                />
+                <h3 className='text_button'>
+                    Ver mis reportes
+                </h3>
+            </div>
+
+            <div className='button_section background_orange'>
+                <img
+                    className='image_button'
+                    src={ImageViewProduct}
+                    alt="ver mis productos"
+                />
+                <h3 className='text_button'>
+                    Ver mis productos
+                </h3>
+            </div>
+
+            <div className='button_section background_orange'>
+                <img
+                    className='image_button'
+                    src={ImageViewWaiters}
+                    alt="ver mis meseros"
+                />
+                <h3 className='text_button'>
+                    Ver meseros
+                </h3>
+            </div>
+
+            <div className='button_section background_orange'>
+                <img
+                    className='image_button'
+                    src={ImageViewCashier}
+                    alt="ver mis cajeros"
+                />
+                <h3 className='text_button'>
+                    Ver cajeros
+                </h3>
+            </div>
+
+            <div className='button_section background_orange'>
+                <img
+                    className='image_button'
+                    src={ImageViewZone}
+                    alt="ver mis zonas"
+                />
+                <h3 className='text_button'>
+                    Ver zonas
+                </h3>
+            </div>
+
+        </WrapperHomePage>
      );
 }
  
---
+++ src/components/Homepage/HomePage.styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const WrapperHomePage = styled.div`
+    display         : flex;
+    margin          : 0 auto;
+    flex-direction  : column;
+    margin-bottom   : 30px;
+
+    .button_section{
+
+        width           : 85%;
+        display         : flex;
+        height          : 100px;
+        flex-direction  : row;
+        border-radius   : 10px;
+        margin          : 0 auto;
+        margin-top      : 30px;
+        align-items     : center;
+        cursor          : pointer;
+
+        .image_button{
+            width       : 64px ;
+            height      : 64px ;
+            margin-left : 22px;
+        }
+
+        .text_button{
+            font-family : 'system-ui';
+            font-style  : normal;
+            font-weight : 700;
+            font-size   : 24px;
+            line-height : 29px;
+            color       : #FFFFFF;
+            margin-left : 33px;
+        }
+    }
+
+    .background_black{
+        background  : #000000;
+    }
+    .background_blue{
+        background  : #046EBB;
+    }
+    .background_orange{
+        background  : #FF7629;
+    }
+`
